90050c9b6c13a76ae6aef24c39aaa53b43344177
kylo2001 <kylo2001@naver.com>
2022-05-03 16:30:00 +0900

    감정 추출을 위한 동영상 업로드 구현

--- Face_Flow/Face_Flow/settings.py
+++ Face_Flow/Face_Flow/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media folder
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/'

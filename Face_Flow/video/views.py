7c4a6b51e3ae18ec8eb4a6b786063564edac16fe
kylo2001 <kylo2001@naver.com>
2022-05-05 14:34:15 +0900

    모델 적용

--- Face_Flow/video/views.py
+++ Face_Flow/video/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
 from .forms import UploadFileForm
-from django.views.decorators.csrf import ensure_csrf_cookie
-import torch
-import os
 
-from django.http import FileResponse
 from django.core.files.storage import FileSystemStorage
+from django.http import FileResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+import os
 
 
 def downloadFile(request, param):
@@ -18,52 +18,39 @@ def downloadFile(request, param):
     response = FileResponse(fs.open(file_name, 'rb'),
                             content_type='video/mp4')
     response['Content-Disposition'] = f'attachment; filename="{param}"'
-    
+
     return response
+
+
 @ensure_csrf_cookie
 def upload_display_video(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             file = request.FILES['file']
-            # print(file.name)
             handle_uploaded_file(file)
-
-            # model 적용 시도
-            # model = torch.hub.load(
-            #     'ultralytics/yolov3', 'custom', path='0502_epoch_100.pt', force_reload=True)
-            # xmin, ymin, xmax, ymax, confidence, class_num, name = model(
-            #     "IMG_4485.PNG").pandas().xyxy[0].values.tolist()[0]
 
             # Remove existing folders (runs/detect/)
             cmd = f"rm -rf ../yolov3/runs/detect/"
-            returned_value = os.system(cmd)
+            os.system(cmd)
 
             # Detection
             cmd = f"python ../yolov3/detect.py --source {file.name}"
-            cmd_weights = ' --weights 0502_epoch_100.pt'
-            #cmd = cmd + cmd_weights
-            returned_value = os.system(cmd)
+            cmd_weights = ' --weights 0505_epoch_46.pt'
+
+            # cmd = cmd + cmd_weights
+            cmd = cmd + cmd_weights
+            os.system(cmd)
             response = {}
             response['filename'] = file.name
 
             # Move video file
             file_name_output = f"{(file.name).split('.')[0]}_output.mp4"
             cmd_mv = f"mv ../yolov3/runs/detect/exp/{file.name} ./{file_name_output}"
-            returned_value = os.system(cmd_mv)
+            os.system(cmd_mv)
             response['filename_output'] = file_name_output
 
             return render(request, "index.html", response)
-            # return render(request, "upload-display-video.html", {
-            #     'filename': file.name,
-            #     'xmin': xmin,
-            #     'ymin': ymin,
-            #     'xmax': xmax,
-            #     'ymax': ymax,
-            #     'confidence': confidence,
-            #     'class_num': class_num,
-            #     'name': name,
-            # })
     else:
         form = UploadFileForm()
 
